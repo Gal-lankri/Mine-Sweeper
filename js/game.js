1ea4e1f4ec9f2007cc316431530bf77c4f4e625d
Gal-lankri <108870341+Gal-lankri@users.noreply.github.com>
2022-09-22 20:07:32 +0300

    final upload

---
+++ js/game.js
@@ -0,0 +1,183 @@
+"use strict"
+
+const EASY_LEVEL = 4
+const HURD_LEVEL = 8
+const EXTREME_LEVEL = 12
+
+const FLAG = "🚩"
+const MINE = "💣"
+const NUM1 = "1️"
+const NUM2 = "2️"
+const NUM3 = "3️"
+const EMPTY = ""
+const HAPPY_FACE = "😃"
+const SHOCK_FACE = "😲"
+const COOL_FACE = "😎"
+
+var gBoard
+var gInterval
+var gFirstClick = 0
+
+const G_GAME = {
+  isOn: false,
+  shownCount: 0,
+  markedCount: 0,
+  secsPassed: 0,
+}
+const gLevel = {
+  size: 4,
+  mines: 2,
+}
+
+function initGame() {
+  gFirstClick = 0
+  var selGameOver = document.querySelector(".game-over")
+  selGameOver.innerText = HAPPY_FACE
+  clearInterval(gInterval)
+
+  G_GAME.isOn = true
+  gBoard = buildBoard(gLevel.size)
+
+  renderBoard(gBoard, ".board-container")
+
+  console.log(gBoard)
+}
+
+// build the model
+
+function buildBoard(size) {
+  const board = []
+  for (var i = 0; i < size; i++) {
+    board[i] = []
+    for (var j = 0; j < size; j++) {
+      board[i][j] = {
+        minesAroundCount: 0,
+        isShown: false,
+        isMine: false,
+        isMarked: false,
+      }
+    }
+  }
+  getRandomMines(board, gLevel)
+  // setting the Neighbors of the mines
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      if (board[i][j].isMine) {
+        setMinesNegsCount(board, i, j)
+      }
+    }
+  }
+
+  return board
+}
+// update the DOM
+function renderBoard(gBoard, selector) {
+  var strHTML = '<table border="3"><tbody>'
+  for (var i = 0; i < gBoard.length; i++) {
+    strHTML += "<tr>"
+    for (var j = 0; j < gBoard[0].length; j++) {
+      const cell = gBoard[i][j]
+      const className = "cell cell-" + i + "-" + j
+      if (cell.isMine) {
+        strHTML += `<td class="${className}" onclick="cellClicked(this,${i},${j})" oncontextmenu="event.preventDefault();" onmouseup="cellMarked(this, event, ${i}, ${j})"><span class="hidden-content">${MINE}</span><span class="hidden-flag">${FLAG}</span></td>`
+      } else if (cell.minesAroundCount > 0) {
+        strHTML += `<td class="${className}" onclick="cellClicked(this,${i},${j})" oncontextmenu="event.preventDefault();" onmouseup="cellMarked(this, event, ${i}, ${j})"><span class="hidden-content">${cell.minesAroundCount}</span><span class="hidden-flag">${FLAG}</span></td>`
+      } else {
+        strHTML += `<td class="${className}" onclick="cellClicked(this,${i},${j})" oncontextmenu="event.preventDefault();" onmouseup="cellMarked(this, event, ${i}, ${j})"><span class="hidden-content">${EMPTY}</span><span class="hidden-flag">${FLAG}</span></td>`
+      }
+    }
+    strHTML += "</tr>"
+  }
+  strHTML += "</tbody></table>"
+
+  const elContainer = document.querySelector(selector)
+  elContainer.innerHTML = strHTML
+}
+
+function checkGameOver() {
+  // prettier-ignore
+  if (G_GAME.markedCount === gLevel.mines && G_GAME.shownCount === (gLevel.size * gLevel.size) - gLevel.mines) {
+    console.log(G_GAME.markedCount)
+    return true
+  } else {return false}
+}
+
+function cellClicked(elCell, i, j) {
+  G_GAME.shownCount++
+  console.log(G_GAME.shownCount)
+  // if step on a mine
+  if (gBoard[i][j].isMine) {
+    clearInterval(gInterval)
+    var selGameOver = document.querySelector(".game-over")
+    selGameOver.innerText = SHOCK_FACE
+  }
+
+  //  Marked cell can't be clicked
+  elCell.classList.add("marked")
+  if (gBoard[i][j].isMarked) return
+  // Checked if it is te first click
+  gFirstClick++
+  if (gFirstClick === 1) {
+    setTimer()
+  }
+
+  // Checked if it is empty cell
+  if (
+    elCell.innerHTML === `<span class="hidden-content"></span><span class="hidden-flag">🚩</span>`
+  ) {
+    expandShown(gBoard, elCell, i, j)
+  }
+
+  // update model
+  gBoard[i][j].isShown = true
+  // update DOM
+  var elSpan = elCell.getElementsByTagName("span")
+  elSpan[0].classList.remove("hidden-content")
+
+  if (checkGameOver()) {
+    debugger
+    // if win !
+    clearInterval(gInterval)
+    var selGameOver = document.querySelector(".game-over")
+    selGameOver.innerText = COOL_FACE
+  }
+}
+function cellMarked(elCell, ev, i, j) {
+  // checked if it is the right button on the mouse
+  if (ev.button === 2) {
+    G_GAME.markedCount++
+    console.log(G_GAME.markedCount)
+    console.log(ev)
+    if (gBoard[i][j].isShown) return
+    gBoard[i][j].isMarked = true
+    var elSpan = elCell.getElementsByTagName("span")
+    elSpan[1].classList.remove("hidden-flag")
+  }
+}
+
+function chooseLevel(level) {
+  if (level === EASY_LEVEL) {
+    gLevel.size = EASY_LEVEL
+    gLevel.mines = 2
+  }
+  if (level === HURD_LEVEL) {
+    gLevel.size = HURD_LEVEL
+    gLevel.mines = 14
+  }
+  if (level === EXTREME_LEVEL) {
+    gLevel.size = EXTREME_LEVEL
+    gLevel.mines = 32
+  }
+  initGame()
+}
+
+function setTimer() {
+  var timer = document.querySelector(".timer-container")
+  var start = Date.now()
+  gInterval = setInterval(() => {
+    var currTime = Date.now()
+    var seconds = parseInt((currTime - start) / 1000)
+    G_GAME.secsPassed = seconds
+    timer.innerText = `Time :\n ${seconds}`
+  }, 1000)
+}
--- js/mines.js
+++ js/mines.js
@@ -1,68 +1,55 @@
 "use strict"
-const MINE = "💣"
+
 var gMines = []
 
-function getRandomMines() {
+function getRandomMines(board, level) {
   var prevRandomIdx = { i: -1, j: -1 }
-  for (var i = 0; i < gLevel.mines; i++) {
+  for (var i = 0; i < level.mines; i++) {
     var randomIdx = {
-      i: getRandomIntInclusive(0, gLevel.size - 1),
-      j: getRandomIntInclusive(0, gLevel.size - 1),
+      i: getRandomIntInclusive(0, level.size - 1),
+      j: getRandomIntInclusive(0, level.size - 1),
     }
-    if (randomIdx.i === prevRandomIdx.i && randomIdx.j === prevRandomIdx.j) continue
-    gBoard[randomIdx.i][randomIdx.j].isMine = true
+    if (randomIdx.i === prevRandomIdx.i && randomIdx.j === prevRandomIdx.j) {
+      i--
+      continue
+    }
+    board[randomIdx.i][randomIdx.j].isMine = true
     prevRandomIdx.i = randomIdx.i
     prevRandomIdx.j = randomIdx.j
   }
 }
 
-function renderMines(board) {
-  for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board[i].length; j++) {
-      var currCellLocation = { i, j }
-      if (board[i][j].isMine) {
-        renderCell(currCellLocation, getMineHTML())
-      } else {
-       
-        setMinesNegsCount(board, i, j)
-      }
-    }
-  }
-}
-
-function renderCell(location, value) {
-
-  var elCell = document.querySelector(`.cell-${location.i}-${location.j}`)
-  console.log(elCell)
-  elCell.innerHTML = value
-}
-
 function setMinesNegsCount(board, rowIdx, colIdx) {
-  var minesCount = 0
-
   for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
     if (i < 0 || i >= board.length) continue
     for (var j = colIdx - 1; j <= colIdx + 1; j++) {
       if (i === rowIdx && j === colIdx) continue
       if (j < 0 || j >= board[0].length) continue
-      var currCell = board[rowIdx][colIdx].isMine
-      if (currCell) minesCount++
+      if (board[i][j].isMine) continue
+      var currCell = board[i][j]
+
+      currCell.minesAroundCount++
     }
   }
+}
 
-  if (minesCount > 0) {
-    //   Update model
-    board[rowIdx][colIdx].minesAroundCount = minesCount
-    //   Update DOM
-    renderCell({ rowIdx, colIdx }, getMinesNegsHTML(minesCount))
+function expandShown(board, elCell, rowIdx, colIdx) {
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= board.length) continue
+
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= board[0].length) continue
+      if (i === rowIdx && j === colIdx) continue
+      if (board[i][j].isMine) return
+      var currCell = board[i][j]
+      if (!currCell.isShown) {
+        currCell.isShown = true
+        G_GAME.shownCount++
+      }
+      var currCellEl = document.querySelector(`.cell-${i}-${j}`)
+      currCellEl.classList.add("marked")
+      var elSpan = currCellEl.getElementsByTagName("span")
+      elSpan[0].classList.remove("hidden-content")
+    }
   }
-}
-
-function getMineHTML() {
-  return `<span class="mine">${MINE}</span>`
-}
-function getMinesNegsHTML(mineNegs) {
-  if (mineNegs === 1) return `<span class="negs">${NUM1}</span>`
-  if (mineNegs === 2) return `<span class="negs">${NUM2}</span>`
-  if (mineNegs === 3) return `<span class="negs">${NUM3}</span>`
 }
